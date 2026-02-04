86bbef0cd19b63386b41a46e1fcedf04b1f50301
Distance <egoistgray@gmail.com>
2026-02-04 17:48:13 +0700

    fix(tsup): use swc instead of esbuild plugin

--- packages/graphql-annotation/tsup.config.ts
+++ packages/graphql-annotation/tsup.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "tsup";
-import esbuildPluginTsc from "esbuild-plugin-tsc";
 
 export default defineConfig({
   tsconfig: "tsconfig.build.json",
-  esbuildPlugins: [esbuildPluginTsc()],
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   outDir: "dist",
