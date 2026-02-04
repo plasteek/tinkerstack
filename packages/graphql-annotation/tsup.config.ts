ebcfb1acee4709367dab29db349790340adb9a72
Distance <egoistgray@gmail.com>
2026-02-04 17:22:13 +0700

    fix: include decorator in tsup build config

--- packages/graphql-annotation/tsup.config.ts
+++ packages/graphql-annotation/tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsup";
+import esbuildPluginTsc from "esbuild-plugin-tsc";
 
 export default defineConfig({
   tsconfig: "tsconfig.build.json",
+  esbuildPlugins: [esbuildPluginTsc()],
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   outDir: "dist",
